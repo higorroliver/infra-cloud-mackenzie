911056f730d1204a756a930f922c49398737db9d
Higor Rodrigues <higorro.oliveira@gmail.com>
2025-11-10 18:00:51 -0300

    subindo att iniciais

---
+++ projeto3/variables.tf
@@ -0,0 +1,9 @@
+variable tags {
+    description = "Tags aplicação"
+    default {
+        Name = "WebServer"
+        EmailOwner = "higorro.oliveira@gmail.com"
+        Environment = "Production Lab"
+    }
+}
+
